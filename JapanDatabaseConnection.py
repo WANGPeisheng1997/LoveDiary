fb689d777b77b3e1a8c1ff63e15b61fbcfc4bcc3
王沛晟 <115681033@qq.com>
2018-08-21 17:07:44 +0800

    add routes and database connection
    add routes and database connection

---
+++ JapanDatabaseConnection.py
@@ -0,0 +1,67 @@
+# -*-coding:utf-8-*-
+import pymysql
+import dbconfig
+
+
+class JapanConnection(object):
+    def connect_database(self):
+        self.connection = pymysql.connect(host=dbconfig.host,
+                                          user=dbconfig.user,
+                                          db=dbconfig.japandb,
+                                          passwd=dbconfig.passwd,
+                                          port=dbconfig.port,
+                                          charset=dbconfig.charset
+                                          )
+        self.cursor = self.connection.cursor()
+
+    def disconnect_database(self):
+        self.cursor.close()
+        self.connection.close()
+
+    def exec_query(self, sql):
+        self.cursor.execute(sql)
+
+    def exec_update(self, sql):
+        self.cursor.execute(sql)
+        self.connection.commit()
+
+    def fetch_cursor(self):
+        return self.cursor.fetchall()
+
+
+japan_database = JapanConnection()
+
+
+def exec_add_account(description, currency, cost, spenderid, date, time, typeid):
+    sql = "INSERT INTO account(account_description, account_currency, account_cost, account_spenderid, account_date, account_time, account_typeid) " \
+          "VALUES('%s','%s','%s','%s','%s','%s','%s')"
+    data = (description, currency, cost, spenderid, date, time, typeid)
+    japan_database.exec_update(sql % data)
+
+
+def exec_delete_account(accountid):
+    sql = "DELETE FROM account WHERE account_id='%d'"
+    japan_database.exec_update(sql % accountid)
+
+
+def exec_fetch_all_accounts():
+    sql = "SELECT account_id, account_date, account_time, account_description, account_currency, account_cost, spender_name, type_name " \
+          "FROM account, spender, account_type " \
+          "WHERE account.account_spenderid=spender.spender_id AND account.account_typeid=account_type.type_id"
+    japan_database.exec_query(sql)
+    accounts = japan_database.fetch_cursor()
+    return accounts
+
+
+def exec_fetch_all_types():
+    sql = "SELECT * FROM account_type"
+    japan_database.exec_query(sql)
+    type_list = japan_database.fetch_cursor()
+    return type_list
+
+
+def exec_fetch_all_spenders():
+    sql = "SELECT * FROM spender"
+    japan_database.exec_query(sql)
+    spender_list = japan_database.fetch_cursor()
+    return spender_list
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8-*-
 
 import DatabaseConnection
+import JapanDatabaseConnection
 from flask import Flask, render_template, request, redirect, url_for, flash
 from datetime import datetime
 import time
@@ -10,12 +11,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('/moneymanagement')
+    return redirect('/japan/accounting')
 
 
 @app.route('/japan/accounting', methods=['GET'])
 def japan_accounting():
-    return render_template('japan_accounting.html')
+    JapanDatabaseConnection.japan_database.connect_database()
+    accounts = JapanDatabaseConnection.exec_fetch_all_accounts()
+    types = JapanDatabaseConnection.exec_fetch_all_types()
+    spenders = JapanDatabaseConnection.exec_fetch_all_spenders()
+    JapanDatabaseConnection.japan_database.disconnect_database()
+    return render_template('japan_accounting.html', account_list=accounts, type_list=types, spender_list=spenders)
+
+
+@app.route('/japan/accounting/<post_type>', methods=['POST'])
+def japan_process_form_post(post_type=None):
+    JapanDatabaseConnection.japan_database.connect_database()
+    if post_type == "add":
+        description = request.form['description']
+        currency = request.form['currency']
+        cost = request.form['cost']
+        spenderid = request.form['spender']
+        date = request.form['date']
+        time = request.form['time']
+        typeid = request.form['type']
+        JapanDatabaseConnection.exec_add_account(description, currency, cost, spenderid, date_web_to_sql(date), time_web_to_sql(time),
+                                            typeid)
+
+    if post_type == "delete":
+        accountid = int(request.form['accountid'])
+        JapanDatabaseConnection.exec_delete_account(accountid)
+    JapanDatabaseConnection.japan_database.disconnect_database()
+    return redirect('/japan/accounting')
 
 
 @app.route('/japan/schedule', methods=['GET'])
@@ -23,7 +50,7 @@ def japan_schedule():
     return render_template('japan_schedule.html')
 
 
-@app.route('/japan/schedule', methods=['GET'])
+@app.route('/japan/maps', methods=['GET'])
 def japan_maps():
     return render_template('japan_maps.html')
 
